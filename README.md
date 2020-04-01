fd103565636e17a486265ea64f8dc9d4ea888341
ivanka <ivanka.caron@gmail.com>
2020-04-01 14:55:34 +0200

    second commit

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ http://127.0.0.1:5000/view
 
 https://classroom.github.com/g/aPMtL8ai
 https://github.com/Simplon-IA-Biarritz-1/intro-a-flask-ivanka
+
+
+to run:
+python tuto.py
+
+anaconda:
+conda create -n introFlask python=3.7
+conda activate introFlask
+
+pythonanywhere:
+python3.7 -m venv env
+source ./env/bin/activate 
+
+pip install -r requirements.txt
+(pip install SQLAlchemy
+pip install flask_sqlalchemy
+)
--- tuto.py
+++ tuto.py
@@ -2,9 +2,18 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 
+import json
+import plotly
+import plotly.express as px
+
+import pandas as pd
+import numpy as np
+
+
 app = Flask(__name__)
 
 app.secret_key = "HelloWorld"
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'ivanka0164.mysql.pythonanywhere-services.com'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config["SQLALCHEME_TRACK_MODIFICATIONS"] = False
 app.permanent_session_lifetime = timedelta(minutes=20)
@@ -12,6 +21,7 @@ db = SQLAlchemy(app)
 
 
 class users(db.Model):
+
     _id = db.Column("id", db.Integer, primary_key = True)
     name = db.Column( db.String(100))
     email = db.Column( db.String(100))
@@ -24,6 +34,16 @@ class users(db.Model):
 @app.route("/")
 def home():
     return render_template('index.html', content="Testing")
+
+#exo 1
+@app.route("/exo1")
+def hello():
+    return "Hello World!"
+
+#exo 2 & 3
+@app.route("/exo2_3")
+def hello2():
+    return render_template("exo2_3.html", message = "Hello Multicolor World! I hope you like this amazing and uselessly long rainbow sentence , i add a black color:)")
 
 @app.route("/view")
 def view():
@@ -89,7 +109,77 @@ def logout():
     return redirect(url_for("login"))
 
 
+# Exo 7
+@app.route("/exo7")
+def exo7():
+    return render_template("exo7.html")
+
+
+@app.route("/exo7/dataset", methods=["POST"])
+def exo7_dataset_describe():
+    fichier = request.files.get("dataset")
+   
+    # TODO Recupérer le séparateur grâce à des Regex
+    sep = request.form["sep"]
+
+    # TODO Extraire le nom du fichier grâce à des Regex
+    name = str(fichier)[15:-15]
+
+    df = pd.read_csv(fichier, sep=sep)
+    describ = round(df.describe(),2)
+
+    dico = {}
+    for x in describ:
+        dico[x] = describ[x].to_dict()
+
+    # TODO Régler problème quand 1ere entete est catégorielle
+    a = list(dico.keys())[0]
+    entete = list(dico[a].keys())
+
+    return render_template("exo7.html", name=name, entete=entete, data=dico)
+
+
+
+
+# Exo 7 avec plotly 
+#df_sample = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/minoritymajority.csv')
+
+gapminder = px.data.gapminder()
+fig = px.scatter(gapminder.query("year==2007"), x="gdpPercap", y="lifeExp", size="pop", color="continent",
+           hover_name="country", log_x=True, size_max=60)
+#fig.show()
+
+
+fig.layout.template = None
+graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+@app.route("/plotly")
+def home1():
+    return render_template('plotly.html', v = graphJSON)
+
+
+
+
+# Exo 7 avec plotly (plusier graphs)
+df = px.data.gapminder()
+fig2 = px.line_geo(df.query("year==2007"), locations="iso_alpha", color="continent", projection="orthographic")
+fig2.layout.template = None
+    # Convert the figures to JSON
+    # PlotlyJSONEncoder appropriately converts pandas, datetime, etc
+    # objects to their JSON equivalents
+graphJSON2 = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+@app.route('/plotly2')
+def plotly2():
+
+    return render_template('plotly2.html',
+                           graphJSON2=graphJSON2 )
+
+
+
+
+
 if __name__ == "__main__":
     db.create_all()
     app.run(debug=True)
+    #app.run(host='0.0.0.0', port=9999)
 
